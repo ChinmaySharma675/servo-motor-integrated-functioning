dfb966736cfc860f26664c087722f32b6012492a
ChinmaySharma675 <chinmay.sharma675@gmail.com>
2026-02-09 10:03:10 +0530

    main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <Arduino.h>
+#include<Servo.h>
+Servo myServo;
+#define SERVO_PIN 9
 void setup() {
 // write your initialization code here
+    myServo.attach(SERVO_PIN);
+    Serial.begin(9600);
 }
 
 void loop() {
 // write your code here
+    myServo.write(SERVO_PIN);
+    delay(1000);
+    myServo.write(0);
+    delay(1000);
+    myServo.write(120);
 }
